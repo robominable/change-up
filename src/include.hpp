f197caa6072df34cd5ac431f6fce2e428cd8d62b
Todd Rylaarsdam <todd@toddr.org>
2020-10-28 16:54:39 -0500

    ports v1

--- src/include.hpp
+++ src/include.hpp
@@ -3,11 +3,11 @@
 using namespace pros;
 
 pros::Controller master(E_CONTROLLER_MASTER);
-pros::Motor FLdrive (1, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor FLdrive (10, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES); //correct as of 10/28/2020
 pros::Motor FRdrive (2, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor BLdrive (3, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor BRdrive (4, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor LfrontIntake (5, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor RfrontIntake (6, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor flywheel (7, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor mainIntake (8, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor flywheel (19, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES); //correct as of 10/28/2020
+pros::Motor mainIntake (11, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES); //correct as of 10/28/2020
